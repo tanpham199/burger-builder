7e1859d36087ca8035aa0fbe91b28d990c5098fe
tanpham199 <tanpham1104@gmail.com>
2020-12-12 12:41:48 +0700

    feat: finish routing and minor html fixs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
-import Layout from './containers/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom';
+
+import Layout from './containers/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path={"/checkout"} component={Checkout} />
+          <Route path={"/orders"} component={Orders} />
           <Route path={"/"} exact component={BurgerBuilder} />
         </Switch>
       </Layout>
